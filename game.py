3b0cd8fe7bc922b2dc54e3ec7451dd6f2c6c38ed
lukePlawson <m273618@usna.edu>
2024-11-22 14:43:19 -0500

    Edited start and formatting doc

--- game.py
+++ game.py
@@ -58,10 +58,8 @@ land = pygame.image.load('assets2/Tiles/tile_0001.png')
 track = pygame.image.load('assets2/Tiles/tile_0130.png')
 TILE_SIZE = land.get_width() #can be called for width or height of tiles b/c squares
 
-#Create a truck instance at the Start line of the track
+#Create a truck at the Start line of the track
 truck = Truck(410, 67)
-
-
 
 #Start button stuff
 start = pygame.image.load('assets3/Sprites/flat-dark/flatDark41.png')
@@ -70,7 +68,8 @@ start_b = start.get_rect(center=(WIDTH//2,HEIGHT//2))
 
 #Countdown for the race
 countdown=10
-cdown= False
+cdown = False  # Set to True when countdown starts
+start_clicked = False  # Track if the start button was clicked
 
 
 #Run the program to display and update pygame
@@ -81,7 +80,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill background tiles based on log_list above
+
+
+    if event.type == pygame.MOUSEBUTTONDOWN: #When start button gets pressed start countdown
+        if start_b.collidepoint(event.pos):
+            cdown = True #Starts countdown
+    
+
+
+
+
+#############   BACKGROUND    #########################
+    #Fill background tiles based on log_list above
     for y in range(len(log_list)):  
         for x in range(len(log_list[y])):
             tile = log_list[y][x]
@@ -91,34 +101,20 @@ while running:
             elif tile == 't':
                 background.blit(track,(x*TILE_SIZE, y*TILE_SIZE))
 
-
     #Blits start button on the screen after the background is in
-    background.blit(start_b)
+    if not start_clicked:
+        background.blit(start, start_b)
+#######################################################
 
-
-    #Once start button gets clicked it gets deleted
-
-
-
-    #Runs the countdown after the start button disappears
-
-
-
+##############    CONSTANT UPDATES    ################
     # Get key presses for truck movement
     keys = pygame.key.get_pressed()
 
-    # Update the truck's position based on keys
-    truck.update(keys)
 
-    #blits the screen constantly
-    screen.blit(background, (0, 0))  # Redraw the background
-
-    #Blits the truck on the screen
-    truck.blit(screen)
-
-    #Updates the display
-    pygame.display.flip()
-    #Limits FPS to 60
-    clock.tick(60)
+    truck.update(keys)      # Update the truck's position
+    screen.blit(background, (0, 0))   #Blits the truck on the screen
+    truck.blit(screen)     #Blits the truck on the screen
+    pygame.display.flip()       #Updates the display
+    clock.tick(60)    #Limits FPS to 60
 
 pygame.quit()
